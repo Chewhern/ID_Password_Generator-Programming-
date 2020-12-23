34e1cf6439d4d90834764e1693b611fb4e09f591
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2020-12-23 14:14:15 +0800

    Update CryptographicSecureIDGenerator.cs

--- C# or VB.Net/Cryptographic_Secure/CryptographicSecureIDGenerator.cs
+++ C# or VB.Net/Cryptographic_Secure/CryptographicSecureIDGenerator.cs
@@ -15,12 +15,14 @@ namespace SecureRandomByteArrayDataGenerator
             Byte[] CryptographicSecureData = new Byte[240];
             RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
             rngCsp.GetBytes(CryptographicSecureData);
+            //If you don't use sodium library, you can proceed by deleting all lines from 19-22
             Byte[] HashedBytes = GenericHash.Hash(CryptographicSecureData, null,64);
             HashedBytes = GenericHash.Hash(HashedBytes, null, 64);
             HashedBytes = GenericHash.Hash(HashedBytes, null, 64);
             HashedBytes = GenericHash.Hash(HashedBytes, null, 64);
             int Loop = 0;
             StringBuilder stringBuilder = new StringBuilder();
+            //All possible HashedBytes needs to be replaced by CryptographicSecureData variable from line 26 to line 38
             while (Loop < HashedBytes.Length)
             {
                 if (HashedBytes[Loop] >= 48 && HashedBytes[Loop] <= 57)
